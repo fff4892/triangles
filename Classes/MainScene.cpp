f09706c7f9a8ca3ea84e8e866e12175292aa6216
fff4892 <fff4892@gmail.com>
2018-10-01 22:18:46 +0300

    Add background

--- Classes/MainScene.cpp
+++ Classes/MainScene.cpp
@@ -13,5 +13,14 @@ bool MainScene::init()
 		return false;
 	}
 
+	auto visibleSize = Director::getInstance()->getVisibleSize();
+	Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+	auto bgSprite = Sprite::create("background.png");
+	if (bgSprite) {
+		bgSprite->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
+		this->addChild(bgSprite, 0);
+	}
+
 	return true;
 }
